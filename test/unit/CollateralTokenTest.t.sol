3ac0f503a43be8f183304863ec7e41bd401eb67b
devak07 <andrzejknapikprogramming@gmail.com>
2025-01-20 23:32:39 +0100

    Core of the protocol  added: Collateral Token and Stability Engine, furthemore basic deploy script is added. PROJECT IS UNTESTED YET. Attempt #3 due to failure of last commit

--- test/unit/CollateralTokenTest.t.sol
+++ test/unit/CollateralTokenTest.t.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
 
+import {Test, console} from "forge-std/Test.sol";
+
+contract CollateralTokenTest is Test {
+    function setUp() external {
+        console.log("Setting up CollateralTokenTest");
+    }
+
+    function testToNotFailures() external {
+        console.log("Testing to not failures");
+    }
+}
